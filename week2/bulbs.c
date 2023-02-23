7bf78083578c33769059356b883d59f92c90478d
max power <lifeontherun26.2@gmail.com>
2023-02-23 14:16:35 -0700

    Update bulbs.c

--- week2/bulbs.c
+++ week2/bulbs.c
@@ -1,1 +1,55 @@
 #include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+const int BITS_IN_BYTE = 8;
+
+void print_bulb(int bit);
+
+int main(void)
+{
+    // TODO: Prompt user for a message
+    string message = get_string("Message: ");
+
+    // Iterate through each character
+    for (int i = 0, n = strlen(message); i < n; i++)
+    {
+        int binary[] = {0, 0, 0, 0, 0, 0, 0, 0};
+
+        // Convert each character to ASCII
+        int decimal = message[i];
+
+        // Convert ASCII to binary
+        int j = 0;
+
+        while (decimal > 0)
+        {
+            binary[j] = decimal % 2;
+            decimal = decimal / 2;
+            j++;
+        }
+
+        // Print binary in reverse
+        for (int k = BITS_IN_BYTE - 1; k >= 0; k--)
+        {
+            print_bulb(binary[k]);
+        }
+        printf("\n");
+    }
+
+    // Print binary to bulbs; 1 == yellow, 0 == black
+}
+
+void print_bulb(int bit)
+{
+    if (bit == 0)
+    {
+        // Dark emoji
+        printf("\U000026AB");
+    }
+    else if (bit == 1)
+    {
+        // Light emoji
+        printf("\U0001F7E1");
+    }
+}
