067c8b35a1a3b5c459cb365b2e2eb259b5c2f9e4
max power <lifeontherun26.2@gmail.com>
2023-03-21 09:22:49 -0700

    Update log.sql

--- week7/fiftyville/log.sql
+++ week7/fiftyville/log.sql
@@ -1,83 +1,131 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
---All you know is that the theft took place on July 28, 2020 and that it took place on Chamberlin Street.
+-- Checking the description of the crime happened at the given location and time.
 SELECT description
-FROM crime_scene_reports
-WHERE day=28 AND month=7 AND year=2020 AND street="Chamberlin Street"
+  FROM crime_scene_reports
+ WHERE year = 2021
+   AND month = 7
+   AND day = 28
+   AND street = 'Humphrey Street';
 
---Work with the description
-SELECT transcript
-FROM interviews
-WHERE day = "28" AND month = "7" AND year = "2020" AND transcript like "%courthouse%"
+-- Two incidents happened that day. Only one is related to theft. Other is related to littering.
 
---Working with the first transcript:
---"Sometime within ten minutes of the theft, I saw the thief get into a car in the courthouse
---parking lot and drive away. If you have security footage from the courthouse parking lot,
---you might want to look for cars that left the parking lot in that time frame."
+-- The witnesses could be the accomplice. So, finding the names of the witnesses from the interviews table. Also, checking their interviews' transcripts.
+SELECT name, transcript
+  FROM interviews
+ WHERE year = 2021
+   AND month = 7
+   AND day = 28;
+
+-- Found two transcripts with the name- Eugene, so checking how many Eugenes are present in the 'people' table.
 SELECT name
-FROM people
-JOIN courthouse_security_logs ON people.license_plate = courthouse_security_logs.license_plate
-WHERE day = "28" AND month = "7" AND year = "2020" AND hour = "10" AND minute >= "15" AND minute < "25" AND activity = "exit"
+  FROM people
+ WHERE name = 'Eugene';
+-- Found out that there is only one Eugene. So, proceeding accordingly.
 
---Suspects: Patrick, Ernest, Amber, Danielle, Roger, Elizabeth, Russel, Evelyn
+-- Findling the names of the 3 witnesses from the list of names of people who gave interviews on July 28, 2021. Crime report said that the witnesses mentioned "bakery" in their transcripts. Also, ordering the results alphabetically by the names of witnesses.
+SELECT name, transcript
+  FROM interviews
+ WHERE year = 2021
+   AND month = 7
+   AND day = 28
+   AND transcript LIKE '%bakery%'
+ ORDER BY name;
+-- Witnesses are- Eugene, Raymond, and Ruth.
 
 
---Working with the second transcript:
---"I don't know the thief's name, but it was someone I recognized. Earlier this morning,
---before I arrived at the courthouse, I was walking by the ATM on Fifer Street and saw
---the thief there withdrawing some money."
-SELECT DISTINCT name
-FROM people
-JOIN bank_accounts ON people.id = bank_accounts.person_id
-JOIN atm_transactions ON bank_accounts.account_number = atm_transactions.account_number
-WHERE day = "28" AND month = "7" AND year = "2020" AND transaction_type = "withdraw" AND atm_location = "Fifer Street"
+-- Eugene gave clues- Thief was withdrawing money from the ATM on Leggett Street. So, checking the account number of the person who did that transaction.
+SELECT account_number, amount
+  FROM atm_transactions
+ WHERE year = 2021
+   AND month = 7
+   AND day = 28
+   AND atm_location = 'Leggett Street'
+   AND transaction_type = 'withdraw';
+-- Finding the names associated with the corresponding account numbers. Putting these names in the 'Suspect List'
+SELECT name, atm_transactions.amount
+  FROM people
+  JOIN bank_accounts
+    ON people.id = bank_accounts.person_id
+  JOIN atm_transactions
+    ON bank_accounts.account_number = atm_transactions.account_number
+ WHERE atm_transactions.year = 2021
+   AND atm_transactions.month = 7
+   AND atm_transactions.day = 28
+   AND atm_transactions.atm_location = 'Leggett Street'
+   AND atm_transactions.transaction_type = 'withdraw';
 
---Suspects: Danielle, Bobby, Madison, Ernest, Roy, Elizabeth, Victoria, Russel
 
---By now, the names that appeared in both cases are: Ernest, Danielle, Elizabeth, Russel
+-- Raymond gave clues-- As leaving the bakery, they called a person and talked for less than a minute. They asked the person on the other end of the call to buy a flight ticket of the earliest flight on July 29, 2021.
+-- First finding the information about the aiport in Fiftyville which would be the origin of the flight of the thief.
+SELECT abbreviation, full_name, city
+  FROM airports
+ WHERE city = 'Fiftyville';
+-- Finding the flights on July 29 from Fiftyville airport, and ordering them by time.
+SELECT flights.id, full_name, city, flights.hour, flights.minute
+  FROM airports
+  JOIN flights
+    ON airports.id = flights.destination_airport_id
+ WHERE flights.origin_airport_id =
+       (SELECT id
+          FROM airports
+         WHERE city = 'Fiftyville')
+   AND flights.year = 2021
+   AND flights.month = 7
+   AND flights.day = 29
+ ORDER BY flights.hour, flights.minute;
+-- First flight comes out to be at 8:20 to LaGuardia Airport in New York City (Flight id- 36). This could be the place where the thief went to.
+-- Checking the list of passengers in that flight. Putting them all in 'Suspect List'. Ordering the names according to their passport numbers.
+SELECT passengers.flight_id, name, passengers.passport_number, passengers.seat
+  FROM people
+  JOIN passengers
+    ON people.passport_number = passengers.passport_number
+  JOIN flights
+    ON passengers.flight_id = flights.id
+ WHERE flights.year = 2021
+   AND flights.month = 7
+   AND flights.day = 29
+   AND flights.hour = 8
+   AND flights.minute = 20
+ ORDER BY passengers.passport_number;
+-- Checking the phone call records to find the person who bought the tickets.
+-- Firstly, checking the possible names of the caller, and putting these names in the 'Suspect List'. Ordering them according to the durations of the calls.
+SELECT name, phone_calls.duration
+  FROM people
+  JOIN phone_calls
+    ON people.phone_number = phone_calls.caller
+ WHERE phone_calls.year = 2021
+   AND phone_calls.month = 7
+   AND phone_calls.day = 28
+   AND phone_calls.duration <= 60
+ ORDER BY phone_calls.duration;
+-- Secondly, checking the possible names of the call-receiver. Ordering them according to the durations of the calls.
+SELECT name, phone_calls.duration
+  FROM people
+  JOIN phone_calls
+    ON people.phone_number = phone_calls.receiver
+ WHERE phone_calls.year = 2021
+   AND phone_calls.month = 7
+   AND phone_calls.day = 28
+   AND phone_calls.duration <= 60
+   ORDER BY phone_calls.duration;
 
---Working with the third transcript:
---"As the thief was leaving the courthouse, they called someone who talked to them for less than a minute.
---In the call, I heard the thief say that they were planning to take the earliest flight out of Fiftyville
---tomorrow.The thief then asked the person on the other end of the phone to purchase the flight ticket."
---First check the passengers for the flight
-SELECT name
-FROM people
-JOIN passengers ON people.passport_number = passengers.passport_number
-WHERE flight_id = (
-    SELECT id
-    FROM flights
-    WHERE day = "29" AND month = "7" AND year = "2020"
-    ORDER BY hour, minute
-    LIMIT 1)
---Suspects: Doris, Roger, Ernest, Edward, Evelyn, Madison, Bobby, Danielle
---By now, our main suspects are: Ernest and Danielle
 
---Check the phone calls
-SELECT DISTINCT name
-FROM people
-JOIN phone_calls ON people.phone_number = phone_calls.caller
-WHERE day = "28" AND month = "7" AND year = "2020" AND duration < "60"
---Suspects: Roger, Evelyn, Ernest, Madison, Russell, Kimberly, Bobby, Victoria
+-- Ruth gave clues- The thief drove away in a car from the bakery, within 10 minutes from the theft. SO, checking the license plates of cars within that time frame. Then, checking out the names of those cars' owners. They could be suspects.
+SELECT name, bakery_security_logs.hour, bakery_security_logs.minute
+  FROM people
+  JOIN bakery_security_logs
+    ON people.license_plate = bakery_security_logs.license_plate
+ WHERE bakery_security_logs.year = 2021
+   AND bakery_security_logs.month = 7
+   AND bakery_security_logs.day = 28
+   AND bakery_security_logs.activity = 'exit'
+   AND bakery_security_logs.hour = 10
+   AND bakery_security_logs.minute >= 15
+   AND bakery_security_logs.minute <= 25
+ ORDER BY bakery_security_logs.minute;
 
---The thief is Ernest!
 
-SELECT city
-FROM airports
-WHERE id = (
-    SELECT destination_airport_id
-    FROM flights
-    WHERE day = "29" AND month = "7" AND year = "2020"
-    ORDER BY hour, minute
-    LIMIT 1)
---The thief ESCAPED TO: London
-
---Now we have to get "The ACCOMPLICE is":
-SELECT name
-FROM people
-JOIN phone_calls ON people.phone_number = phone_calls.receiver
-WHERE day = "28" AND month = "7" AND year = "2020" AND duration < "60" AND caller = (
-    SELECT phone_number
-    FROM people
-    WHERE name = "Ernest");
---The ACCOMPLICE is: Berthold
+-- Bruce must the thief as he is present in all the 4 lists- List of passengers, list of people who did the specific atm transactions, list of people who called, and list of people who drove away in cars from the bakery.
+-- He must have escaped to the New York City, as he took the New York City flight.
+-- Robin must be the accomplice who purchased the flight ticket, and helped Bruce escape to the New York City.
